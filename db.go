c2338d11fa7a02c9fb4254bca2489f7d6267316c
Edward Tirado Jr <edward.tirado@outlook.com>
2017-09-24 10:18:44 -0600

    Set up docker

--- db.go
+++ db.go
@@ -12,11 +12,10 @@ import (
 
 func Database() *gorm.DB {
     var login string = os.Getenv("FITQUESTDB")
-    var db_string string = fmt.Sprintf("%s@/fitquest?charset=utf8&parseTime=True&loc=Local", login)
+    var db_string string = fmt.Sprintf("%s@tcp(fitquestdb:3306)/fitquest?charset=utf8&parseTime=True&loc=Local", login)
 
     db, err :=  gorm.Open("mysql", db_string)
     if err != nil {
-        //panic("Failed to connect to database")
         log.Fatal(err)
     }
     return db
