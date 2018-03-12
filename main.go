b173bb5d9df90fe005077ed6a0b02da347604929
Edward Tirado Jr <edward.tirado@outlook.com>
2018-03-11 22:19:38 -0600

    All workouts now use the same model.  Added sets.

--- main.go
+++ main.go
@@ -11,14 +11,14 @@ import (
 
 func main() {
     db := Database()
-    db.AutoMigrate(&Strength{}, &Cardio{})
+    db.AutoMigrate(&Routine{},&Workout{},&Set{})
 
     router := gin.Default()
 
     v1 := router.Group("/api/v1/workouts")
     {
         v1.GET("/", GetWorkouts)
-        v1.GET("/:id/:type", GetWorkout)
+        v1.GET("/:id", GetWorkout)
         v1.PUT("/:id/:type", UpdateWorkout)
         v1.POST("/create", CreateWorkout)
         v1.DELETE("/:id/:type", DeleteWorkout)
@@ -28,37 +28,46 @@ func main() {
 
 
 func CreateWorkout(c *gin.Context) {
+    var sets []Set
+
     name := c.PostForm("name")
     workout_type := c.PostForm("type")
-    set,_ := strconv.Atoi(c.PostForm("set"))
+    sets = GenerateSets()
     exp,_ := strconv.Atoi(c.PostForm("exp"))
     distance,_ := strconv.Atoi(c.PostForm("distance"))
     time,_ := strconv.Atoi(c.PostForm("time"))
     weight,_ := strconv.Atoi(c.PostForm("weight"))
-    reps,_ := strconv.Atoi(c.PostForm("reps"))
+    increase_by,_ := strconv.Atoi(c.PostForm("increase_by"))
 
     db := Database()
 
     if workout_type == "cardio" {
-        var cardio Cardio
+        var cardio Workout
 
-        cardio = Cardio{
-            Workout: Workout{Name: name, Set: set, Experience: exp},
+        cardio = Workout{
+            Name: name,
+            Sets: sets,
             Distance: distance,
             Time: time,
+            Experience: exp,
         }
         db.Create(&cardio)
+        c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "message": "Workout Created!", "workout": cardio})
 
     } else if workout_type == "strength" {
-        var strength Strength
+        var strength Workout
 
-        strength = Strength {
-            Workout: Workout{Name: name, Set: set, Experience: exp},
+        strength = Workout{
+            Name: name,
+            Type: workout_type,
+            Sets: sets,
             Weight: weight,
-            Reps: reps,
+            IncreaseBy: increase_by,
+            Experience: exp,
         }
 
         db.Create(&strength)
+        c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "message": "Workout Created!", "workout": strength})
     } else {
         db.Close()
         c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "message": "No Workout Found"})
@@ -66,16 +75,13 @@ func CreateWorkout(c *gin.Context) {
 
     db.Close()
 
-    c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "message": "Workout Created!"})
     return
 }
 
 
 func GetWorkout(c *gin.Context) {
-    var cardio Cardio
-    var strength Strength
+    var workout Workout
 
-    workout_type := c.Param("type")
     id, err := strconv.Atoi(c.Param("id"))
     if err != nil {
         log.Println("Id not provided")
@@ -83,47 +89,35 @@ func GetWorkout(c *gin.Context) {
 
     db := Database()
 
-    if workout_type == "cardio" {
-        if db.First(&cardio,id).RecordNotFound() {
-            c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "workout":"No Workout Found"})
-            return
-        }
-        c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "workout":cardio})
-    } else if workout_type == "strength" {
-        if db.First(&strength,id).RecordNotFound() {
-            c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "workout":"No Workout Found"})
-            return
-        }
-        c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "workout":strength})
-    } else {
-        c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "message": "No Workout Found"})
+    if db.Preload("Sets").First(&workout,id).RecordNotFound() {
+        c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "workout":"No Workout Found"})
+        return
     }
+    c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "workout":workout})
 
     return
 }
 
 
 func GetWorkouts(c *gin.Context) {
-    var cardios []Cardio
-    var strengths []Strength
+    var workouts []Workout
 
     db := Database()
-    db.Find(&cardios)
-    db.Find(&strengths)
+    db.Preload("Sets").Find(&workouts)
 
-    c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "cardio": cardios, "strength": strengths})
+    c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "workouts":workouts})
     return
 }
 
 
 func UpdateWorkout(c *gin.Context) {
-    var cardio Cardio
-    var strength Strength
+    var cardio Workout
+    var strength Workout
 
     id := c.Param("id")
     workout_type := c.Param("type")
     name := c.PostForm("name")
-    set,_ := strconv.Atoi(c.PostForm("set"))
+    sets := GenerateSets()
     exp,_ := strconv.Atoi(c.PostForm("exp"))
 
     db := Database()
@@ -138,7 +132,7 @@ func UpdateWorkout(c *gin.Context) {
         }
 
         cardio.Name = name
-        cardio.Set = set
+        cardio.Sets= sets
         cardio.Experience = exp
         cardio.Distance = distance
         cardio.Time = time
@@ -148,7 +142,6 @@ func UpdateWorkout(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "workout":cardio, "message": "Workout saved!"})
     } else if workout_type == "strength" {
         weight,_ := strconv.Atoi(c.PostForm("weight"))
-        reps,_ := strconv.Atoi(c.PostForm("reps"))
 
         if db.First(&strength,id).RecordNotFound() {
             c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "workout":"No Workout Found"})
@@ -156,9 +149,8 @@ func UpdateWorkout(c *gin.Context) {
         }
 
         strength.Name = name
-        strength.Set = set
+        strength.Sets = sets
         strength.Weight = weight
-        strength.Reps = reps
 
         db.Save(&strength)
 
@@ -172,30 +164,28 @@ func UpdateWorkout(c *gin.Context) {
 
 
 func DeleteWorkout(c *gin.Context) {
-    var cardio Cardio
-    var strength Strength
+    var workout Workout
 
     id := c.Param("id")
-    workout_type := c.Param("type")
 
     db := Database()
 
-    if workout_type == "cardio" {
-        if db.First(&cardio, id).RecordNotFound() {
-            c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "workout":"No Workout Found"})
-            return
-        }
-        db.Delete(&cardio)
-    } else if workout_type == "strength" {
-        if db.First(&strength, id).RecordNotFound() {
-            c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "workout":"No Workout Found"})
-            return
-        }
-        db.Delete(&strength)
-    } else {
-        c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "message": "No Workout Found"})
+    if db.First(&workout, id).RecordNotFound() {
+        c.JSON(http.StatusNotFound, gin.H{"status":http.StatusNotFound, "workout":"No Workout Found"})
+        return
     }
+    db.Delete(&workout)
 
     c.JSON(http.StatusOK, gin.H{"status":http.StatusOK, "workout":"Workout Deleted!"})
     return
+}
+
+
+func GenerateSets() []Set {
+    /* Just for testing.  DELETE THIS */
+    var set_test []Set
+    set_test = append(set_test, Set{Reps:5,IncreaseWeight:true})
+    set_test = append(set_test, Set{Reps:3,IncreaseWeight:false})
+
+    return set_test
 }
--- model.go
+++ model.go
@@ -4,24 +4,43 @@ import (
     "github.com/jinzhu/gorm"
 )
 
+type Weekday string
+
+const (
+    Monday Weekday = "Monday"
+    Tuesday Weekday = "Tuesday"
+    Wednesday Weekday = "Wednesday"
+    Thursday Weekday = "Thursday"
+    Saturday Weekday = "Saturday"
+    Sunday Weekday = "Sunday"
+)
+
+
+type Routine struct {
+    gorm.Model
+    Name string `json:"routine_name"`
+    Day Weekday `json:"weekday"`
+    Workouts []Workout `json:"workouts"`
+}
+
 
 type Workout struct {
     gorm.Model
-    Name string `json:"name"`
-    Set int `json:"set"`
+    RoutineId int
+    Name string `json:"workout_name"`
+    Type string `json:"workout_type"`
+    Sets []Set `json:"sets"`
+    Distance int `json:"distance"`
+    Time int `json:"time"`
+    Weight int `json:"weight"`
+    IncreaseBy int `json:"increase_by"`
     Experience int `json:"exp"`
 }
 
 
-type Cardio struct {
-    Workout
-    Distance int `json:"distance"`
-    Time int `json:"time"`
-}
-
-
-type Strength struct {
-    Workout
-    Weight int `json:"weight"`
-    Reps int `json:"reps"`
+type Set struct {
+    gorm.Model
+    WorkoutId int
+    Reps int
+    IncreaseWeight bool
 }
